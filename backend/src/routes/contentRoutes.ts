1e1473da693ae4ccf917185260f85adee32ef029
aman-4321 <amanclash4321@gmail.com>
2024-11-27 00:05:54 +0530

    added functionality for deleting and getting content by id

--- backend/src/routes/contentRoutes.ts
+++ backend/src/routes/contentRoutes.ts
@@ -102,3 +102,111 @@ contentRouter.get(
     }
   },
 );
+
+contentRouter.get(
+  "/get/:id",
+  authMiddleware,
+  async (req: Request, res: Response) => {
+    const userId = Number(req.userId);
+    const contentId = Number(req.params.id);
+
+    if (isNaN(contentId)) {
+      res.status(400).json({ message: "Invalid content ID" });
+      return;
+    }
+
+    try {
+      const content = await prisma.content.findUnique({
+        where: { id: contentId },
+        select: {
+          id: true,
+          link: true,
+          type: true,
+          title: true,
+          tags: {
+            select: {
+              id: true,
+              title: true,
+            },
+          },
+          userId: true,
+        },
+      });
+
+      if (!content) {
+        res.status(404).json({
+          message: "Content not found",
+        });
+        return;
+      }
+
+      if (content.userId !== userId) {
+        res.status(403).json({
+          message: "You don't have permission to access this content",
+        });
+        return;
+      }
+
+      res.status(200).json({
+        content,
+      });
+      return;
+    } catch (err) {
+      res.status(500).json({
+        message: "Unable to fetch content",
+        err,
+      });
+    }
+  },
+);
+
+contentRouter.delete(
+  "/delete/:id",
+  authMiddleware,
+  async (req: Request, res: Response) => {
+    const userId = Number(req.userId);
+    const contentId = Number(req.params.id);
+
+    if (isNaN(contentId)) {
+      res.status(400).json({ message: "Invalid content ID" });
+      return;
+    }
+
+    try {
+      const content = await prisma.content.findUnique({
+        where: { id: contentId },
+      });
+
+      if (!content) {
+        res.status(404).json({
+          message: "Content not found",
+        });
+        return;
+      }
+
+      if (content.userId !== userId) {
+        res.status(403).json({
+          message: "You do not have permission to delete this content",
+        });
+        return;
+      }
+
+      await prisma.content.delete({
+        where: {
+          id: contentId,
+        },
+      });
+
+      res.status(200).json({
+        message: "Content deleted successfully",
+        id: contentId,
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({
+        message: "Unable to delete the content",
+        err,
+      });
+    }
+  },
+);
--- backend/src/routes/shareRoutes.ts
+++ backend/src/routes/shareRoutes.ts
@@ -1,3 +1,12 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import { authMiddleware } from "../middleware/middleware";
 
 export const shareRouter = Router();
+
+shareRouter.post("/open", authMiddleware, (req: Request, res: Response) => {});
+
+shareRouter.post(
+  "/openall",
+  authMiddleware,
+  (req: Request, res: Response) => {},
+);
